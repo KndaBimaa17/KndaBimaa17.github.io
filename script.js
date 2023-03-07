eb93092edf8f0ae13093ad08e3ba13395c481e35
KndaBimaa17 <muhammadkanda100505@gmail.com>
2023-03-07 09:56:57 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  alert(
+    "hai keyziaa, diisi semuanya yaa, tiap halaman di submit dulu baru lanjut kehalaman selanjutnya okaii, jangan cuek2 amat kalo bisa ashasha, havefunn!"
+  );
+};
+// window.onload = function () {
+//   alert(
+//     "oiya im ready nya kan ada 3, diisi smua ya"
+//   );
+// };
