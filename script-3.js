35847d456f7573feff89b63628041eba6fa934c1
KndaBimaa17 <muhammadkanda100505@gmail.com>
2023-03-10 15:38:13 +0700

    Final Result.
    cape anjay ternyata tp fun siih asikk even ditempuh dengan berbagai drama wkwkwk...

---
+++ script-3.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  alert(
+    "haii keyziaa, gakerasa udah selesai aja yaa huhuu, btw ini buatnya kurang lebih 8-10 jam an karenaa aku masih belajar juga dan yaa seruu dan gakerasa karena buatnya buat kezii, semoga sukaa dan terimakasii sudah menceritakan smuanyaa!!"
+  );
+  form.submit();
+});
